0fd4931529dbf8b9ec3113338eb64b211a6142b8
xDCSEx <ashleydoidge01@gmail.com>
2024-04-29 15:40:11 +0100

    Day 4 projects

---
+++ Day 4/Coin Toss.py
@@ -0,0 +1,12 @@
+import random
+
+Coin_toss = random.randint(0, 1)
+
+
+print(Coin_toss)
+if Coin_toss == 0:
+    print("Heads")
+else:
+    print("Tails")
+
+    
---
+++ Day 4/Main.py
@@ -0,0 +1,18 @@
+import random
+
+random_integer = random.randint(1,100)
+
+# print(random_integer)
+
+random_float = random.random()
+
+print(random_float)
+
+random_float * 5
+
+print(random_float)
+
+love_score = random.randint(1, 100)
+
+print(f"Your Love score is {love_score}")
+
